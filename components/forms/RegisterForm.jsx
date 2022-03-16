b0a46be6c5c51166b4191d5d760dd9d866a55680
Beygs <boris.gilles@hotmail.fr>
2022-03-16 12:41:13 +0100

    register form validations

--- components/forms/RegisterForm.jsx
+++ components/forms/RegisterForm.jsx
@@ -1,6 +1,16 @@
-import { USERNAME_REGEX } from "lib/contants";
+import { USERNAME_REGEX } from "lib/constants/validations";
 import { useEffect, useRef, useState } from "react";
-import { form, inputWrapper, input, btn } from "./form.module.scss";
+import {
+  form,
+  inputWrapper,
+  input,
+  btn,
+  errmsg,
+  offscreen,
+  instructions,
+} from "./form.module.scss";
+import cn from "classnames";
+import { EMAIL_REGEX, PWD_REGEX } from "../../lib/constants/validations";
 
 const LoginForm = () => {
   const errors = useRef();
@@ -30,9 +40,23 @@ const LoginForm = () => {
 
   const usernameValidation = () => {
     const result = USERNAME_REGEX.test(username.current.value);
-    console.log(username.current.value, result);
     setValidUsername(result);
   };
+
+  const emailValidation = () => {
+    const result = EMAIL_REGEX.test(email.current.value);
+    setValidEmail(result);
+  };
+
+  const pwdValidation = () => {
+    const result = PWD_REGEX.test(pwd.current.value);
+    setValidPwd(result);
+  }
+
+  const pwdConfirmValidation = () => {
+    const result = pwd.current.value === pwdConfirm.current.value;
+    setValidPwdConfirm(result);
+  }
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -55,7 +79,7 @@ const LoginForm = () => {
 
       <p
         ref={errors}
-        className={errMsg ? "errmsg" : "offscreen"}
+        className={cn(errmsg, { [offscreen]: !errMsg })}
         aria-live="assertive"
       >
         {errMsg}
@@ -79,10 +103,18 @@ const LoginForm = () => {
         <label htmlFor="username-input">Nom d&apos;utilisateur</label>
         <p
           id="uidnote"
-          className={usernameFocus && username && !validUsername ? "instructions" : "offscreen"}
+          className={cn(instructions, {
+            [offscreen]: !(
+              usernameFocus &&
+              username &&
+              !validUsername
+            ),
+          })}
         >
-          Au moins 6 caractères.<br/>
-          Doit commencer par une lettre<br/>
+          Au moins 6 caractères.
+          <br />
+          Doit commencer par une lettre
+          <br />
           Caractères autorisés: lettres, nombres, tirets (- et _).
         </p>
       </div>
@@ -95,8 +127,26 @@ const LoginForm = () => {
           placeholder=" "
           ref={email}
           autoComplete="email"
+          required
+          aria-invalid={validEmail ? "false" : "true"}
+          aria-describedby="emailnote"
+          onChange={emailValidation}
+          onFocus={() => setEmailFocus(true)}
+          onBlur={() => setEmailFocus(false)}
         />
         <label htmlFor="email-input">Email</label>
+        <p
+          id="emailnote"
+          className={cn(instructions, {
+            [offscreen]: !(
+              emailFocus &&
+              email &&
+              !validEmail
+            ),
+          })}
+        >
+          Veuillez entrer un email valide.
+        </p>
       </div>
 
       <div className={inputWrapper}>
@@ -107,8 +157,26 @@ const LoginForm = () => {
           placeholder=" "
           ref={pwd}
           autoComplete="new-password"
+          required
+          aria-invalid={validPwd ? "false" : "true"}
+          aria-describedby="pwdnote"
+          onChange={pwdValidation}
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
         />
         <label htmlFor="password-input">Mot de passe</label>
+        <p
+          id="pwdnote"
+          className={cn(instructions, {
+            [offscreen]: !(
+              pwdFocus &&
+              pwd &&
+              !validPwd
+            ),
+          })}
+        >
+          Au moins 6 caractères.
+        </p>
       </div>
 
       <div className={inputWrapper}>
@@ -118,11 +186,29 @@ const LoginForm = () => {
           id="passwordConfirm-input"
           placeholder=" "
           ref={pwdConfirm}
-          autoComplete="off"
+          autoComplete="new-password"
+          required
+          aria-invalid={validPwdConfirm ? "false" : "true"}
+          aria-describedby="pwdconfirmnote"
+          onChange={pwdConfirmValidation}
+          onFocus={() => setPwdConfirmFocus(true)}
+          onBlur={() => setPwdConfirmFocus(false)}
         />
         <label htmlFor="passwordConfirm-input">
           Confirmation du mot de passe
         </label>
+        <p
+          id="pwdconfirmnote"
+          className={cn(instructions, {
+            [offscreen]: !(
+              pwdConfirmFocus &&
+              pwdConfirm &&
+              !validPwdConfirm
+            ),
+          })}
+        >
+          Veuillez réécrire votre mot de passe.
+        </p>
       </div>
 
       <input className={btn} type="submit" role="button" value="M'inscrire" />
