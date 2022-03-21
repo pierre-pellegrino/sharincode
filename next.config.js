003a8a6a947f5af086b88dae3f128b59c65a4fcb
Beygs <boris.gilles@hotmail.fr>
2022-03-22 00:07:21 +0100

    basic pwa setup

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: [
       "staging-xs3.herokuapp.com",
       "snipshare-api-staging.herokuapp.com",
       "snipshare-api.herokuapp.com",
-      "localhost"
+      "localhost",
     ],
-  }
-}
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html lang="fr">
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#ffffff" />
           <link
             href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;700&family=Work+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap"       
             rel="stylesheet"
