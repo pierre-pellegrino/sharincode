1adde2c07ff763be7856bd586ef07eaf32af1b93
Beygs <boris.gilles@hotmail.fr>
2022-03-22 00:21:51 +0100

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
@@ -6,10 +6,29 @@ class MyDocument extends Document {
       <Html lang="fr">
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="apple-touch-icon" href="/icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffa000" />
+          <meta name="msapplication-TileColor" content="#2b5797" />
           <meta name="theme-color" content="#ffffff" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;700&family=Work+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap"       
+            href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;700&family=Work+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap"
             rel="stylesheet"
           />
         </Head>
@@ -18,8 +37,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
