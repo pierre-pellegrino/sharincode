bf65e9c7cefa14fb109c2efdd0d98941d514fdad
Beygs <boris.gilles@hotmail.fr>
2022-03-22 10:57:28 +0100

    baseURL changed

--- pages/api/axios.js
+++ pages/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 // const baseurl = "https://snipshare-api-staging.herokuapp.com";
-// const baseurl = 'https://snipshare-api.herokuapp.com'
-const baseurl = 'https://staging-xs3.herokuapp.com'
+const baseurl = 'https://snipshare-api.herokuapp.com'
+// const baseurl = 'https://staging-xs3.herokuapp.com'
 
 const APIRequest = axios.create({ baseURL: baseurl });
 
