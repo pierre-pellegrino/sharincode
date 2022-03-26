21dd8b23c5432c2fb082ebdbcee900aa84e306b4
Beygs <boris.gilles@hotmail.fr>
2022-03-26 21:38:06 +0100

    swr on search page to allow data mutate on reactions changes

--- pages/search.js
+++ pages/search.js
@@ -2,35 +2,41 @@ import styles from "styles/Home.module.scss";
 import Loader from "components/Loader";
 import PostCard from "components/PostCard/PostCard";
 import { useRouter } from "next/router";
-import { useState } from "react";
 import APIManager from "./api/axios";
 import Head from "next/head";
 import { getAbsoluteURL } from "lib/getAbsoluteURL";
 import Link from "next/link";
+import useSWR, { useSWRConfig } from "swr";
+import { useEffect, useState } from "react";
 
 const Search = () => {
   const router = useRouter();
   const { query } = router;
   const [previousQuery, setPreviousQuery] = useState({});
-  const [posts, setPosts] = useState();
 
-  const searchQuery = async () => {
-    try {
-      const response = await APIManager.search(query["q"]);
-      setPosts(response.data.posts);
-    } catch (err) {
-      console.error(err);
+  const { data, error } = useSWR(query !== {} ? "/posts" : null, () =>
+    APIManager.fetcher(`/searches?keywords=${query["q"].replace(" ", "_")}`)
+  );
+
+  const { mutate } = useSWRConfig();
+
+  useEffect(() => {
+    if (query !== {} && query !== previousQuery) {
+      setPreviousQuery(query);
+      mutate("/posts");
     }
-  };
-
-  if (query !== {} && query !== previousQuery) {
-    setPreviousQuery(query);
-    searchQuery();
-  }
+  }, [mutate, previousQuery, query]);
 
   let content = <Loader />;
 
-  if (posts) {
+  if (error) {
+    console.log(error);
+    content = <div>Oups ! Une erreur est survenue...</div>;
+  }
+
+  if (data) {
+    const { posts } = data;
+
     content = posts.map((post) => (
       <PostCard post={post.post} key={post.post.id} />
     ));
@@ -45,15 +51,12 @@ const Search = () => {
         <meta property="og:url" content={getAbsoluteURL(router.asPath)} />
         <meta property="og:title" content="Recherche | Snipshare" />
 
-        <meta
-          property="twitter:url"
-          content={getAbsoluteURL(router.asPath)}
-        />
+        <meta property="twitter:url" content={getAbsoluteURL(router.asPath)} />
         <meta property="twitter:title" content="Recherche | Snipshare" />
       </Head>
       <div className={styles.main}>
         Résultats de la recherche : {router.query["q"]}
-        <Link href="/"> 
+        <Link href="/">
           <a className="txt-primary"> Annuler la recherche </a>
         </Link>
         {content}
