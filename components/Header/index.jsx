8a65f9eb5c1c061b8d06255e34befa19c97934ff
Beygs <boris.gilles@hotmail.fr>
2022-04-03 01:44:33 +0200

    locale switch links added

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 import LoggedInNav from "./LoggedInNav";
 import LoggedOutNav from "./LoggedOutNav";
-import { header, navItems, logo, text, skipLink } from "./header.module.scss";
+import {
+  header,
+  navItems,
+  logo,
+  text,
+  skipLink,
+  localeLinks,
+  logoLocales,
+} from "./header.module.scss";
 import { LogoLeftSide, LogoRightSide } from "../icons";
 import { useAtom } from "jotai";
 import { isConnectedAtom } from "store";
 import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
 const Header = () => {
   const [isConnected] = useAtom(isConnectedAtom);
   const { t } = useTranslation();
+  const { asPath } = useRouter();
 
   return (
     <header className={`${header} bg-global-secondary`}>
@@ -18,17 +28,27 @@ const Header = () => {
           {t("skipLink")}
         </a>
         <ul className={navItems} role="navigation">
-          <li>
-            <Link href="/">
-              <a
-                className={logo}
-                aria-label="Snipshare : Accéder à la page d'accueil."
-              >
-                <LogoLeftSide />
-                <span className={text}>Snipshare</span>
-                <LogoRightSide />
-              </a>
-            </Link>
+          <li className={logoLocales}>
+            <div>
+              <Link href="/">
+                <a
+                  className={logo}
+                  aria-label="Snipshare : Accéder à la page d'accueil."
+                >
+                  <LogoLeftSide />
+                  <span className={text}>Snipshare</span>
+                  <LogoRightSide />
+                </a>
+              </Link>
+            </div>
+            <div className={localeLinks}>
+              <Link href={asPath} locale="fr">
+                <a>🇫🇷</a>
+              </Link>
+              <Link href={asPath} locale="en">
+                <a>🇺🇸</a>
+              </Link>
+            </div>
           </li>
           <li>{isConnected ? <LoggedInNav /> : <LoggedOutNav />}</li>
         </ul>
